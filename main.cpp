a4c23a80bc0095d3d256e23977427bf013434397
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-05 18:01:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
 
 int main(){
   int n, a, b;
   cin >> n >> a >> b;
-  // to find the position he is at we can take both a + 1 and n - b to find his position
-  int result = n - max(a+1, n-b) + 1;
-  cout << result << endl;
+  //We can find his position by adding 1 to a (as he will be the next position)
+  // We can also find his position by subtrating b from n (as he will be in the previous position)
+  //However this will give us two different numbers
+  // To tackle this we look at the test cases and we can see that the algorithm for this would be n-max(a+1,n-b)+1
+  int answer = n-max(a+1,n-b)+1;
+  cout << answer << endl;
+}
